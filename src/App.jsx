c9293ec5de9865b96237f9f366aa06d0c36097bc
Immanuel Joy <immanueljoy107@gmail.com>
2026-02-19 12:57:04 -0500

    updated paragraphs

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ const paragraphs = [
   "If you see me fighting with a bear, help the bear. The only way to do great work is to love what you do. The best way to predict the future is to invent it.",
   "Real self confidence does not come from shouting affirmations in the mirror. It comes from shocasing the world irrefutable proof that you are who you say you are.",
   "You can be depressed and still get things done. Its called being an adult. The only limit to our realization of tomorrow will be our doubts of today.",
-  "I am not saying that its impossible to find love all I am saying is that statistically you you have not; from the bottom of my heart I believe that 80 percent of relationship in the world is horseshit. A bunch of people who neve took time to learn how to be alone therefore never learned how to love themself, so you employed some one else to do it.",
+  "I am not saying that its impossible to find love all I am saying is that statistically you you have not; from the bottom of my heart I believe that 80 percent of relationship in the world is horseshit. A bunch of people who never took time to learn how to be alone therefore never learned how to love themself, so you employed some one else to do it.",
   "Accept people as they are, but place them where they belong. You are the CEO of your life. Hire, fire and promote accordingly."
 ];
 
